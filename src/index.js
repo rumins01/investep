2930ba9d2f93c2d5ac67ace9faf5d9a4e5f73115
efete <eternalfete@gmail.com>
2021-10-22 04:23:15 +0900

    add collapsed nav menu and apply responsive classes tightly

--- src/index.js
+++ src/index.js
@@ -54,7 +54,51 @@ window.addEventListener("load", function () {
     },
     { passive: true }
   )
+
+  // window.addEventListener(
+  //   "scroll",
+  //   function (e) {
+  //     e.preventDefault()
+  //     scrollStatus.wheeling = true
+  //     let direction = e.deltaY < 0 ? "up" : "down"
+  //     let currentPageOffset = currentPage * winHeight
+
+  //     if (!scrollStatus.functionCall) {
+  //       if (direction == "down" && currentPage <= sections.length - 2) {
+  //         window.scrollTo(0, currentPageOffset + winHeight)
+  //         currentPage++
+  //       } else if (direction == "down" && currentPage == sections.length - 1) {
+  //         currentPage++
+  //       } else if (direction == "up" && currentPage > 0) {
+  //         window.scrollTo(0, currentPageOffset - winHeight)
+  //         currentPage--
+  //       }
+
+  //       if (
+  //         (currentPage > 0 && theme == "primary") ||
+  //         (currentPage == 0 && theme == "white")
+  //       ) {
+  //         this.setTimeout(
+  //           () => {
+  //             changeNavbarColor()
+  //           },
+  //           direction == "down" ? 400 : 0
+  //         )
+  //       }
+
+  //       scrollStatus.functionCall = true
+  //     }
+  //     window.clearInterval(scrollTimer)
+  //     scrollTimer = window.setTimeout(function () {
+  //       scrollStatus.wheeling = false
+  //       scrollStatus.functionCall = false
+  //     }, 300)
+  //   },
+  //   false
+  // )
 })
+
+function scrollPage(e, scrollStatus, scrollTimer) {}
 
 function changePageNumber(pageNumber) {
   let pageDiff = Math.abs(currentPage - pageNumber)
@@ -81,21 +125,21 @@ function changeNavbarColor() {
 
   if (theme === "primary") {
     for (let i = 0; i < navbar.length; i++) {
-      navbar[i].classList.remove("bg-green-600")
+      navbar[i].classList.remove("bg-primary")
       navbar[i].classList.add("bg-white")
     }
     for (let i = 0; i < navbarTexts.length; i++) {
       navbarTexts[i].classList.remove("text-white")
-      navbarTexts[i].classList.add("text-green-600")
+      navbarTexts[i].classList.add("text-primary")
     }
     for (let i = 0; i < navbarButtons.length; i++) {
       navbarButtons[i].classList.remove("border-white")
       navbarButtons[i].classList.remove("text-white")
       navbarButtons[i].classList.remove("hover:bg-white")
-      navbarButtons[i].classList.remove("hover:text-green-600")
-      navbarButtons[i].classList.add("border-green-600")
-      navbarButtons[i].classList.add("text-green-600")
-      navbarButtons[i].classList.add("hover:bg-green-600")
+      navbarButtons[i].classList.remove("hover:text-primary")
+      navbarButtons[i].classList.add("border-primary")
+      navbarButtons[i].classList.add("text-primary")
+      navbarButtons[i].classList.add("hover:bg-primary")
       navbarButtons[i].classList.add("hover:text-white")
     }
 
@@ -103,23 +147,37 @@ function changeNavbarColor() {
   } else {
     for (let i = 0; i < navbar.length; i++) {
       navbar[i].classList.remove("bg-white")
-      navbar[i].classList.add("bg-green-600")
+      navbar[i].classList.add("bg-primary")
     }
     for (let i = 0; i < navbarTexts.length; i++) {
-      navbarTexts[i].classList.remove("text-green-600")
+      navbarTexts[i].classList.remove("text-primary")
       navbarTexts[i].classList.add("text-white")
     }
     for (let i = 0; i < navbarButtons.length; i++) {
-      navbarButtons[i].classList.remove("border-green-600")
-      navbarButtons[i].classList.remove("text-green-600")
-      navbarButtons[i].classList.remove("hover:bg-green-600")
+      navbarButtons[i].classList.remove("border-primary")
+      navbarButtons[i].classList.remove("text-primary")
+      navbarButtons[i].classList.remove("hover:bg-primary")
       navbarButtons[i].classList.remove("hover:text-white")
       navbarButtons[i].classList.add("border-white")
       navbarButtons[i].classList.add("text-white")
       navbarButtons[i].classList.add("hover:bg-white")
-      navbarButtons[i].classList.add("hover:text-green-600")
+      navbarButtons[i].classList.add("hover:text-primary")
     }
 
     theme = "primary"
   }
+}
+
+function collapsedNavOpen() {
+  let collapsedNav = document.getElementById("collapsed-nav")
+
+  collapsedNav.classList.remove("hidden")
+  collapsedNav.classList.add("flex", "flex-col")
+}
+
+function collapsedNavClose() {
+  let collapsedNav = document.getElementById("collapsed-nav")
+
+  collapsedNav.classList.remove("flex", "flex-col")
+  collapsedNav.classList.add("hidden")
 }
