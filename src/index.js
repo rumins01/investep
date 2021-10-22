3418bed5faa0fbb82c00950e92592bd0ca5772e7
efete <eternalfete@gmail.com>
2021-10-22 13:25:34 +0900

    javascript 정리

--- src/index.js
+++ src/index.js
@@ -1,171 +1,71 @@
 let winHeight = window.innerHeight
-let sections = document.getElementsByTagName("section")
-let currentPage = 0
 let theme = "primary"
 
-window.addEventListener("resize", function () {
-  winHeight = window.innerHeight
-})
-
 window.addEventListener("load", function () {
-  let scrollStatus = {
-    wheeling: false,
-    functionCall: false,
-  }
-  let scrollTimer = false
+  let positionY = window.scrollY
 
-  window.addEventListener(
-    "wheel",
-    function (e) {
-      scrollStatus.wheeling = true
-      let direction = e.deltaY < 0 ? "up" : "down"
-      let currentPageOffset = currentPage * winHeight
+  winHeight = window.innerHeight
 
-      if (!scrollStatus.functionCall) {
-        if (direction == "down" && currentPage <= sections.length - 2) {
-          window.scrollTo(0, currentPageOffset + winHeight)
-          currentPage++
-        } else if (direction == "down" && currentPage == sections.length - 1) {
-          currentPage++
-        } else if (direction == "up" && currentPage > 0) {
-          window.scrollTo(0, currentPageOffset - winHeight)
-          currentPage--
-        }
+  checkAndToggleNavColor()
 
-        if (
-          (currentPage > 0 && theme == "primary") ||
-          (currentPage == 0 && theme == "white")
-        ) {
-          this.setTimeout(
-            () => {
-              changeNavbarColor()
-            },
-            direction == "down" ? 400 : 0
-          )
-        }
+  window.addEventListener("resize", function () {
+    winHeight = window.innerHeight
+    checkAndToggleNavColor()
+  })
 
-        scrollStatus.functionCall = true
-      }
-      window.clearInterval(scrollTimer)
-      scrollTimer = window.setTimeout(function () {
-        scrollStatus.wheeling = false
-        scrollStatus.functionCall = false
-      }, 300)
-    },
-    { passive: true }
-  )
-
-  // window.addEventListener(
-  //   "scroll",
-  //   function (e) {
-  //     e.preventDefault()
-  //     scrollStatus.wheeling = true
-  //     let direction = e.deltaY < 0 ? "up" : "down"
-  //     let currentPageOffset = currentPage * winHeight
-
-  //     if (!scrollStatus.functionCall) {
-  //       if (direction == "down" && currentPage <= sections.length - 2) {
-  //         window.scrollTo(0, currentPageOffset + winHeight)
-  //         currentPage++
-  //       } else if (direction == "down" && currentPage == sections.length - 1) {
-  //         currentPage++
-  //       } else if (direction == "up" && currentPage > 0) {
-  //         window.scrollTo(0, currentPageOffset - winHeight)
-  //         currentPage--
-  //       }
-
-  //       if (
-  //         (currentPage > 0 && theme == "primary") ||
-  //         (currentPage == 0 && theme == "white")
-  //       ) {
-  //         this.setTimeout(
-  //           () => {
-  //             changeNavbarColor()
-  //           },
-  //           direction == "down" ? 400 : 0
-  //         )
-  //       }
-
-  //       scrollStatus.functionCall = true
-  //     }
-  //     window.clearInterval(scrollTimer)
-  //     scrollTimer = window.setTimeout(function () {
-  //       scrollStatus.wheeling = false
-  //       scrollStatus.functionCall = false
-  //     }, 300)
-  //   },
-  //   false
-  // )
+  window.addEventListener("wheel", checkAndToggleNavColor, { passive: true })
 })
 
-function scrollPage(e, scrollStatus, scrollTimer) {}
+function checkAndToggleNavColor() {
+  if (checkNeedNavColorChange()) toggleNavColor()
+}
 
-function changePageNumber(pageNumber) {
-  let pageDiff = Math.abs(currentPage - pageNumber)
-
-  currentPage = pageNumber
+function checkNeedNavColorChange() {
+  let positionY = window.scrollY
 
   if (
-    (currentPage > 0 && theme == "primary") ||
-    (currentPage == 0 && theme == "white")
+    (positionY > winHeight - 64 && theme == "primary") ||
+    (positionY <= winHeight + 64 && theme == "white")
   ) {
-    this.setTimeout(
-      () => {
-        changeNavbarColor()
-      },
-      pageDiff == 1 && theme == "white" ? 0 : 400
-    )
+    return true
+  } else {
+    return false
   }
 }
 
-function changeNavbarColor() {
-  let navbar = document.getElementsByTagName("nav")
-  let navbarTexts = document.getElementsByClassName("navbar-text")
-  let navbarButtons = document.getElementsByClassName("navbar-button")
+function toggleNavColor() {
+  let navbars = [...document.getElementsByTagName("nav")]
+  let navbarTexts = [...document.getElementsByClassName("navbar-text")]
+  let navbarButtons = [...document.getElementsByClassName("navbar-button")]
 
-  if (theme === "primary") {
-    for (let i = 0; i < navbar.length; i++) {
-      navbar[i].classList.remove("bg-primary")
-      navbar[i].classList.add("bg-white")
-    }
-    for (let i = 0; i < navbarTexts.length; i++) {
-      navbarTexts[i].classList.remove("text-white")
-      navbarTexts[i].classList.add("text-primary")
-    }
-    for (let i = 0; i < navbarButtons.length; i++) {
-      navbarButtons[i].classList.remove("border-white")
-      navbarButtons[i].classList.remove("text-white")
-      navbarButtons[i].classList.remove("hover:bg-white")
-      navbarButtons[i].classList.remove("hover:text-primary")
-      navbarButtons[i].classList.add("border-primary")
-      navbarButtons[i].classList.add("text-primary")
-      navbarButtons[i].classList.add("hover:bg-primary")
-      navbarButtons[i].classList.add("hover:text-white")
-    }
+  const navbarClasses = ["bg-primary", "bg-white"]
+  const navbarTextClasses = ["text-primary", "text-white"]
+  const navbarButtonClasses = [
+    "border-primary",
+    "text-primary",
+    "hover:bg-primary",
+    "hover:text-primary",
+    "border-white",
+    "text-white",
+    "hover:bg-white",
+    "hover:text-white",
+  ]
 
-    theme = "white"
-  } else {
-    for (let i = 0; i < navbar.length; i++) {
-      navbar[i].classList.remove("bg-white")
-      navbar[i].classList.add("bg-primary")
-    }
-    for (let i = 0; i < navbarTexts.length; i++) {
-      navbarTexts[i].classList.remove("text-primary")
-      navbarTexts[i].classList.add("text-white")
-    }
-    for (let i = 0; i < navbarButtons.length; i++) {
-      navbarButtons[i].classList.remove("border-primary")
-      navbarButtons[i].classList.remove("text-primary")
-      navbarButtons[i].classList.remove("hover:bg-primary")
-      navbarButtons[i].classList.remove("hover:text-white")
-      navbarButtons[i].classList.add("border-white")
-      navbarButtons[i].classList.add("text-white")
-      navbarButtons[i].classList.add("hover:bg-white")
-      navbarButtons[i].classList.add("hover:text-primary")
-    }
+  navbars.map((navbar) =>
+    navbarClasses.map((navbarClass) => navbar.classList.toggle(navbarClass))
+  )
+  navbarTexts.map((navbarText) =>
+    navbarTextClasses.map((navbarTextClass) =>
+      navbarText.classList.toggle(navbarTextClass)
+    )
+  )
+  navbarButtons.map((navbarButton) =>
+    navbarButtonClasses.map((navbarButtonClass) =>
+      navbarButton.classList.toggle(navbarButtonClass)
+    )
+  )
 
-    theme = "primary"
-  }
+  theme = theme == "primary" ? "white" : "primary"
 }
 
 function collapsedNavOpen() {
